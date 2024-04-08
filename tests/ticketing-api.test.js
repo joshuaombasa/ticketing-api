1c4fbc7b45d5b10f35e797987e6adcf03b987421
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-09 01:00:48 +0300

    initial commit

--- tests/ticketing-api.test.js
+++ tests/ticketing-api.test.js
@@ -14,7 +14,7 @@ beforeEach(async () => {
     }
 })
 
-describe('when there are initially some tickets', () => {
+describe('when there are initially some tickets saved', () => {
     test('tickets are returned as json', async () => {
         const result = await api.get('/api/tickets')
             .expect(200)
@@ -34,6 +34,58 @@ describe('when there are initially some tickets', () => {
             .expect('Content-Type', /application\/json/)
         const names = result.body.map(t => t.name)
         expect(names).toContain(helper.tickets[0].name)
+    })
+})
+
+describe('viewing a specific ticket', () => {
+    test('succeeds when given a valid id', async () => {
+        const ticketsInDb = await helper.ticketsInDb()
+        const response = await api.get(`/api/tickets/${ticketsInDb[0].id}`)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('fails with statuscode 404 if the id is nonexistent', async () => {
+        const nonExistentId = await helper.nonExistentId()
+        const response = await api.get(`/api/tickets/${nonExistentId}`)
+            .expect(404)
+    })
+
+    test('fails with statuscode 400 if the id is invalid', async () => {
+        const invalidId = 'wefwfwf'
+        const response = await api.get(`/api/tickets/${invalidId}`)
+            .expect(400)
+    })
+})
+
+describe('addition of a new ticket', () => {
+    test('succeeds with valid data', async () => {
+        const ticketsAtStart = await helper.ticketsInDb()
+        const response = await api.post('/api/tickets/')
+            .send(helper.validTestTicket)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        const ticketsAtEnd = await helper.ticketsInDb()
+        expect(ticketsAtEnd).toHaveLength(ticketsAtStart.length + 1)
+    })
+
+    test('fails with statuscode 400 if the 400 is invalid', async () => {
+        const ticketsAtStart = await helper.ticketsInDb()
+        const response = await api.post('/api/tickets/')
+            .send(helper.inValidTestTicket)
+            .expect(400)
+        const ticketsAtEnd = await helper.ticketsInDb()
+        expect(ticketsAtEnd).toHaveLength(ticketsAtStart.length)
+    })
+})
+
+describe('deletion of a ticket', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+        const ticketsAtStart = await helper.ticketsInDb()
+        const response = await api.delete(`/api/tickets/${ticketsAtStart[0].id}`)
+            .expect(204)
+        const ticketsAtEnd = await helper.ticketsInDb()
+        expect(ticketsAtEnd).toHaveLength(ticketsAtStart.length - 1)
     })
 })
 
