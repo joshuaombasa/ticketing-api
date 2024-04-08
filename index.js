7b9b0fb3fa9ef84de51f0f0127f1a0ab2ee10086
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-08 17:03:33 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const app = require('./app')
+require('dotenv').config()
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+
+
+app.listen(config.PORT, () => {
+    logger.info(`server running on port ${config.PORT}`)
+})
+
