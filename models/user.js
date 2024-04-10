736f80779128be9f1003ebc2046a98e19c06cafc
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-04-10 13:08:28 +0300

    add user management to api

--- models/user.js
+++ models/user.js
@@ -8,14 +8,14 @@ const userSchema = new mongoose.Schema({
     isAdmin: { type: String, default: false },
 })
 
-userSchema,mongoose.set('toJSON', {
+userSchema.set('toJSON', {
     transform : (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString
         
         delete returnedObject._id
         delete returnedObject.__v
 
-        delete returnedObject.passwordHash
+        // delete returnedObject.passwordHash
     }
 })
 
